174d5ba24215791cd34e4653ade51351a8428715
Micko Matamorosa <micko.matamorosa@boom.camp>
2020-02-20 17:51:48 +0800

    Subject: Added automated-testing
    Body: Allow user to view backend testing
    
    Task: Test all APIs
    
    Issue: Can't get the actual token to compare

--- backend/src/accounts/serializers.py
+++ backend/src/accounts/serializers.py
@@ -8,7 +8,7 @@ from django.contrib.auth.password_validation import validate_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("id", "username", "email")
+        fields = ("email", "id", "username")
 
 
 # Login Serializer
--- backend/src/accounts/tests.py
+++ backend/src/accounts/tests.py
@@ -1,3 +1,21 @@
-# from django.test import TestCase
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase
+from knox.models import AuthToken
 
-# Create your tests here.
+
+class LoginAPITestCase(APITestCase):
+    def test_login(self):
+        data = {"username": "lgp-drg1", "password": "daraga123"}
+        self.user = User.objects.create_user(**data)
+        response = self.client.post("/api/auth/login", data)
+        self.assertEqual(
+            response.data,
+            {
+                "user": {
+                    "id": self.user.id,
+                    "username": self.user.username,
+                    "email": self.user.email,
+                },
+                "token": AuthToken.objects.first(),
+            },
+        )
