61022f802b4c317d36530b253b95ac7498562d3e
Micko Matamorosa <micko.matamorosa@boom.camp>
2020-02-25 18:00:10 +0800

    Subject: Refactor account test

--- backend/src/accounts/tests.py
+++ backend/src/accounts/tests.py
@@ -11,14 +11,17 @@ class TestCaseSetUp(APITestCase):
     def setUp(self):
         self.username = "lgp-drg1"
         self.password = "daraga123"
-        data = {"username": self.username, "password": self.password}
-
-        self.user = User.objects.create_user(**data)
-        self.token = AuthToken.objects.create(self.user)[1]
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password
+        )
 
 
 class AuthenticationTestCase(TestCaseSetUp):
     """Test user authetication"""
+
+    def setUp(self):
+        super().setUp()
+        self.token = AuthToken.objects.create(self.user)[1]
 
     def test_user_authenticated(self):
         self.client.credentials(HTTP_AUTHORIZATION="Token " + self.token)
@@ -65,44 +68,41 @@ class ChangePassWordTestCase(TestCaseSetUp):
         # change password fields
         self.old_password = self.password
         self.new_password = "123daraga"
-
-    def test_change_with_incorrect_password(self):
         self.client.force_authenticate(user=self.user)
 
+    def change_password(self, data):
+        return self.client.patch(reverse("change_password"), data)
+
+    def user_login(self, data):
+        # execute change password
+        self.test_change_with_correct_password()
+
+        return self.client.post(reverse("user-login"), data)
+
+    def test_change_with_incorrect_password(self):
         # change password
-        response = self.client.patch(
-            reverse("change_password"),
-            {"old_password": "incorrect-password", "new_password": self.new_password},
-        )
+        data = {"old_password": "incorrect-password", "new_password": self.new_password}
+        response = self.change_password(data)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_change_with_correct_password(self):
-        self.client.force_authenticate(user=self.user)
-
         # change password
-        response = self.client.patch(
-            reverse("change_password"),
-            {"old_password": self.old_password, "new_password": self.new_password},
-        )
+        data = {"old_password": self.old_password, "new_password": self.new_password}
+        response = self.change_password(data)
 
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        return response
 
     def test_login_old_password(self):
-        # execute change password
-        self.test_change_with_correct_password()
-
         # login using old password
-        login_data = {"username": self.username, "password": self.old_password}
-        response = self.client.post(reverse("user-login"), login_data)
+        data = {"username": self.username, "password": self.old_password}
+
+        response = self.user_login(data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_login_new_password(self):
-        # execute change password
-        self.test_change_with_correct_password()
-
         # login using new password
-        login_data = {"username": self.username, "password": self.new_password}
-        response = self.client.post(reverse("user-login"), login_data)
+        data = {"username": self.username, "password": self.new_password}
+
+        response = self.user_login(data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
--- backend/src/branches/tests.py
+++ backend/src/branches/tests.py
@@ -1,3 +1,15 @@
-# from django.test import TestCase
+# from rest_framework.reverse import reverse
+from rest_framework import status
+from accounts.tests import TestCaseSetUp
 
-# Create your tests here.
+
+class UserBranchTestCase(TestCaseSetUp):
+    def setUp(self):
+        super().setUp()
+
+        self.client.force_authenticate(user=self.user)
+
+    def test_user_branch(self):
+        data = {"name": "Test Branch Name", "address": "Test Branch Address"}
+        response = self.client.post(f"/api/branch/{self.user.id}/", data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
